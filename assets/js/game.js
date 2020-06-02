7a9cd77d14a9d6a91f4a9707994432c9a6b1eef9
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-02 18:36:19 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -84,29 +84,45 @@ var fight = function(enemyName) {
     }
 };
 
-for(var i = 0; i < enemyNames.length; i++){
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators Round " + (i + 1) );
-    } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+var startGame = function() {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators Round " + (i + 1));
+
+            var pickedEnemyName = enemyNames[i];
+
+            enemyHealth = 50;
+            // call fight function with enemy robot
+            fight(pickedEnemyName);
+        } 
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
     }
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // call fight function with enemy robot
-    fight(enemyNames[i]);
-}
+     //play again
+    endGame();
+};
 
-// fight();
+var endGame = function() {
+    if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame()
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+    }
+};
 
-// Game States
-// "Win" - Player robot has defeated all enemy robots
-//      * Fight all enemy robots
-//      * Defeat each enemy robot
-//  "LOSE" - Player robot's health is zero or less
 
 
-// if (promptFight === 'fight' || promptFight === 'FIGHT') {
-
-    // else {
-    //     window.alert("You need to pick a valid option. Try again!")
+startGame();
